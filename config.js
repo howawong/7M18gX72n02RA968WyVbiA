1370b5ea3c92fc54c8665673f7a5d60f1e1a963d
Ho Wa Wong <howa.wong@gmail.com>
2015-04-23 10:25:43 +0800

    config and dao files added

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	dbconnection: function() {
+		return  'mongodb://aftership:aftership1234@ds029217.mongolab.com:29217/howaaftership';
+	}
+}
---
+++ dao.js
@@ -0,0 +1,20 @@
+var config = require('./config');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+//Save Function
+//Example Use: save("USD", "HKD", 0.13);
+function save(from, to, rate) {
+	db = mongoose.createConnection(config.dbconnection());
+	mongoose.model('Currency', new Schema({from: String, to: String, created_at: {type:Date, default:Date.now}, rate: Number}))
+	var Currency = db.model('Currency');
+	var record = new Currency({from: from, to: to, rate: rate})
+	record.save(
+		function(err) {
+			if (err) throw err;
+			console.log("Saved");
+			console.log("Closing Connection");
+			mongoose.disconnect();
+			console.log("Closed");
+		}
+	);
+}
