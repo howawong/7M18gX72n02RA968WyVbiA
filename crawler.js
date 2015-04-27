dc2dd294523779ad9f14e623ea2ffc5d50badbfc
Ho Wa Wong <howa.wong@gmail.com>
2015-04-27 13:47:42 +0800

    crawler added

---
+++ crawler.js
@@ -0,0 +1,21 @@
+var downloader = require('./downloader');
+var cheerio = require('cheerio');
+function _fetch_rate(from, to, callback) {
+	downloader.download("www.xe.com", 80, '/currencyconverter/convert/?Amount=1&From=' + from + '&To=' + to, function(body){
+		$ = cheerio.load(body);
+    	$('td.rightCol').each(function(i, element){
+      	var a = $(this);
+      	var txt = a.text();
+	  	if (txt.indexOf("=") != -1) {
+			rate = 1.0 / parseFloat(txt.split("=")[1].trim().split(" ")[0]);
+			callback(rate);
+	  	}
+		});
+	});
+}
+
+module.exports = {
+	fetch_rate: function(from, to, callback) {
+		return _fetch_rate(from, to, callback);	
+	},
+}
